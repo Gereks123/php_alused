42db434aa13685bf1207daa756933113ba6aa6de
German <german.eksi@khk.ee>
2020-11-24 09:19:14 +0200

    PHP ALUSED Ülesanne 1 lahendus German Eksi

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ Ülesanne 1, German Eksi 24.11.2020
+ */
+
+$nimi = "German";
+$sünniaasta = 2001;
+$tahtkuju = "lõvi";
+
+$kokku = $nimi.'<br>'.$sünniaasta. '<br>'.$tahtkuju. '<br>';
+
+$janes = "(\(\ <br> ( -.-) <br> o_('')('')";
+
+echo $kokku;
+echo "\"It's My Life\" - Dr. Alban<br>";
+echo $janes;
+
+
+
