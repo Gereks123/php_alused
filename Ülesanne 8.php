456cf6bb6434ba4962c872192f13ddb7b0d712c5
German <german.eksi@khk.ee>
2021-01-12 22:20:35 +0200

    Ülesanne 8 lahendus

---
+++ Ülesanne 8.php
@@ -0,0 +1,17 @@
+<?php
+
+date_default_timezone_set('Europe/Tallinn');
+
+//Ajafunktsioonid
+
+echo time();
+
+
+$kasutaja =date('20.03.2023 17:31');
+
+$muut1 = new DateTime('2023-03-20');
+$muut2 = new DateTime('2021-01-12');
+
+
+$muut
+echo "<br>" .$muut;
