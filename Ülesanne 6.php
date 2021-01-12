3782ed781bef571131ea22a6f942691fb08a6647
German <german.eksi@khk.ee>
2021-01-12 20:34:17 +0200

    Ülesanne 6 lahendus

---
+++ Ülesanne 6.php
@@ -0,0 +1,34 @@
+<?php
+#1
+
+for($arv=1;$arv <=100; $arv++) {
+
+}
+#2
+$tarn = 1;
+for($tarn;$tarn <= 10; $tarn++){
+    echo "*";
+}
+
+#3
+$tarn1 = 1;
+for($tarn1;$tarn1 <= 10; $tarn1++){
+    echo "* <br>";
+}
+
+#4
+
+
+
+#5
+for($i=10;$i >= 1; $i--){
+    echo $i .'<br>';
+}
+
+for($vastus=1;$vastus <=100; $vastus++) {
+    if($vastus % 3 == 0){
+        echo $vastus.'.';
+    } else {
+        echo '';
+    }
+}
